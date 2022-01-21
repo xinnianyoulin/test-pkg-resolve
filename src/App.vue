4087085361002cea965e0e7f9cd2df25ef9d8f61
zyl <zyl@wushikj.com>
2022-01-21 11:33:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">{{ text }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { parse, PropDescriptor } from 'vue-docgen-api'
+
+@Component
+export default class App extends Vue {
+  text: string = 'Hello World'
+}
+</script>
+
+<style lang="scss">
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
